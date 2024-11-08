c5e9762f604e7851cd6f259a30cd0ab9d2012a13
rafiald1 <rafialdi0402@gmail.com>
2024-11-08 17:19:39 +0700

    tugas project the daging'z

---
+++ lib/app/modules/menutulang/controllers/tulang_controller.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+
+class MenuTulangController extends GetxController {
+  var items = [
+    {'title': 'Tulang sum-sum', 'price': '\$40.00', 'image': 'sumsum.png'},
+    {'title': 'Tulang Iga', 'price': '\$40.00', 'image': 'Tulang Iga.png'},
+    {'title': 'Belungan', 'price': '\$40.00', 'image': 'belungan.png'},
+  ].obs;
+}
